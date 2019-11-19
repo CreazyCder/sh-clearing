d1da3fa2091eeecef7f2ba3f8e179487cf378c67
boip <boip@LAPTOP-JQ4B2MIH>
2019-11-19 21:56:32 +0800

    clear init

--- shch-clear/shch-clear-client/src/main/java/cn/com/yusys/yusp/service/SettleNotifyFallback.java
+++ shch-clear/shch-clear-client/src/main/java/cn/com/yusys/yusp/service/SettleNotifyFallback.java
@@ -1,23 +1,21 @@
 package cn.com.yusys.yusp.service;
 
+import org.springframework.stereotype.Component;
+
 import cn.com.yusys.yusp.commons.web.rest.dto.ResultDto;
 import cn.com.yusys.yusp.domain.msg.settlenotify.BondSettleNotifyReq;
 import cn.com.yusys.yusp.domain.msg.settlenotify.CashSettleNotifyReq;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 @Component
 public class SettleNotifyFallback implements SettleNotifyClient {
-    private final Logger logger = LoggerFactory.getLogger(SettleNotifyFallback.class);
 
     @Override
-    public ResultDto bondRsp(BondSettleNotifyReq req) {
-        return null;
+    public ResultDto<String> bondRsp(BondSettleNotifyReq req) {
+        return new ResultDto<String>(1,"fail","error");
     }
 
     @Override
-    public ResultDto cashRsp(CashSettleNotifyReq req) {
-        return null;
+    public ResultDto<String> cashRsp(CashSettleNotifyReq req) {
+    	return new ResultDto<String>(1,"fail","error");
     }
 }
