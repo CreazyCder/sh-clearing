5be0f61ef9c0578ea052d911cd84291607064d81
tom <17701057157@qq.com>
2019-11-20 15:47:59 +0800

    asdfadsf

--- shch-cash/shch-cash-core/src/main/java/cn/com/yusys/yusp/service/CashSettleOrderService.java
+++ shch-cash/shch-cash-core/src/main/java/cn/com/yusys/yusp/service/CashSettleOrderService.java
@@ -16,142 +16,162 @@ import com.github.pagehelper.PageHelper;
 import cn.com.yusys.yusp.commons.mapper.QueryModel;
 import cn.com.yusys.yusp.domain.CashAccountBalance;
 import cn.com.yusys.yusp.domain.CashSettleOrder;
+import cn.com.yusys.yusp.domain.msg.settlenotify.CashSettleNotifyReq;
 import cn.com.yusys.yusp.repository.mapper.CashAccountBalanceMapper;
 import cn.com.yusys.yusp.repository.mapper.CashSettleOrderMapper;
 
 @Service
 public class CashSettleOrderService {
-    private Logger logger = LoggerFactory.getLogger(CashSettleOrderService.class);
-    private ObjectMapper objectMapper = new ObjectMapper();
-    @Autowired
-    private CashSettleOrderMapper cashSettleOrderMapper;
-    
-    @Autowired
-    private CashAccountBalanceMapper cashAccountBalanceMapper;
-    
-    @Autowired
-    private AmqpTemplate amqpTemplate;
+	private Logger logger = LoggerFactory.getLogger(CashSettleOrderService.class);
+	private ObjectMapper objectMapper = new ObjectMapper();
+	@Autowired
+	private CashSettleOrderMapper cashSettleOrderMapper;
 
-    public int deleteByPrimaryKey(String serialNum) {
-        return cashSettleOrderMapper.deleteByPrimaryKey(serialNum);
-    }
-    
-    public int insert(CashSettleOrder record) {
-        return cashSettleOrderMapper.insert(record);
-    }
+	@Autowired
+	private CashAccountBalanceMapper cashAccountBalanceMapper;
 
-    public int insertSelective(CashSettleOrder record) {
-        return cashSettleOrderMapper.insertSelective(record);
-    }
+	@Autowired
+	private AmqpTemplate amqpTemplate;
 
-    public CashSettleOrder selectByPrimaryKey(String serialNum) {
-        return cashSettleOrderMapper.selectByPrimaryKey(serialNum);
-    }
+	@Autowired
+	private SettleNotifyClient settleNotifyClient;
+	
+	public int deleteByPrimaryKey(String serialNum) {
+		return cashSettleOrderMapper.deleteByPrimaryKey(serialNum);
+	}
 
-    public int updateByPrimaryKeySelective(CashSettleOrder record) {
-        return cashSettleOrderMapper.updateByPrimaryKeySelective(record);
-    }
+	public int insert(CashSettleOrder record) {
+		return cashSettleOrderMapper.insert(record);
+	}
 
-    public int updateByPrimaryKey(CashSettleOrder record) {
-        return cashSettleOrderMapper.updateByPrimaryKey(record);
-    }
-    
-    public int updateState(String serialNum){
-        return cashSettleOrderMapper.updateState(serialNum);
-    }
-    
-    public int updateStateReturn(String serialNum){
-        return cashSettleOrderMapper.updateStateReturn(serialNum);
-    }
-    
-    public List<CashSettleOrder> selectByModel(QueryModel model) {
-        PageHelper.startPage(model.getPage(), model.getSize());
-        List<CashSettleOrder> list = cashSettleOrderMapper.selectByModel(model);
-        PageHelper.clearPage();
-        return list;
-    }
+	public int insertSelective(CashSettleOrder record) {
+		return cashSettleOrderMapper.insertSelective(record);
+	}
 
-    public int deleteByIds(String serialNums) {
-        return cashSettleOrderMapper.deleteByIds(serialNums);
-    }
-    
-    @Transactional
-    public void updateCashCount(CashDto record) {
-    	logger.debug("资金DVP结算请求指令开始:"+record);
-        try {
-        	CashSettleOrder cashSettleOrder = new CashSettleOrder();
-        	BeanUtils.copyProperties(record, cashSettleOrder);
-        	cashSettleOrder.setCashAccount("ZHa");
-        	cashSettleOrder.setCashAccountName("账户a");
-        	cashSettleOrder.setCashProcStatus("0");
-        	cashSettleOrder.setCashSettleId(("A"+System.currentTimeMillis()).substring(0, 10));
-        	cashSettleOrder.setSerialNum("B"+System.currentTimeMillis());   
-        	cashSettleOrder.setSettleOrderId(record.getSettleOrderId());
-        	cashSettleOrderMapper.insert(cashSettleOrder);  
-        	logger.debug("等级流水成功"+record);
-        	
-        	CashAccountBalance record2 = new CashAccountBalance();
-            record2.setCurrencyAmt(record.getCashSettleAmt());
-            record2.setMemCode(record.getDebitMemId());
-            record2.setHolderAccount(record.getDebitHolderAccount());
-            record2.setTitleCode(record.getCashDebitTitle());
-            record2.setCashAccount("0009");
-            
-            kouKuan(cashSettleOrder.getSerialNum(), record2);
-			
-		} catch (Exception e) {		
+	public CashSettleOrder selectByPrimaryKey(String serialNum) {
+		return cashSettleOrderMapper.selectByPrimaryKey(serialNum);
+	}
+
+	public int updateByPrimaryKeySelective(CashSettleOrder record) {
+		return cashSettleOrderMapper.updateByPrimaryKeySelective(record);
+	}
+
+	public int updateByPrimaryKey(CashSettleOrder record) {
+		return cashSettleOrderMapper.updateByPrimaryKey(record);
+	}
+
+	public int updateState(String serialNum) {
+		return cashSettleOrderMapper.updateState(serialNum);
+	}
+
+	public int updateStateReturn(String serialNum) {
+		return cashSettleOrderMapper.updateStateReturn(serialNum);
+	}
+
+	public List<CashSettleOrder> selectByModel(QueryModel model) {
+		PageHelper.startPage(model.getPage(), model.getSize());
+		List<CashSettleOrder> list = cashSettleOrderMapper.selectByModel(model);
+		PageHelper.clearPage();
+		return list;
+	}
+
+	public int deleteByIds(String serialNums) {
+		return cashSettleOrderMapper.deleteByIds(serialNums);
+	}
+
+	@Transactional
+	public void updateCashCount(CashDto record) {
+		logger.debug("资金DVP结算请求指令开始:" + record);
+		try {
+			CashSettleOrder cashSettleOrder = new CashSettleOrder();
+			BeanUtils.copyProperties(record, cashSettleOrder);
+
+			cashSettleOrder.setCashAccount("ZHa");
+			cashSettleOrder.setCashAccountName("账户a");
+			cashSettleOrder.setCashProcStatus("0");
+			cashSettleOrder.setCashSettleId(("A" + System.currentTimeMillis()).substring(0, 10));
+			cashSettleOrder.setSerialNum("B" + System.currentTimeMillis());
+			cashSettleOrderMapper.insert(cashSettleOrder);
+			logger.debug("等级流水成功" + record);
+
+			CashAccountBalance record2 = new CashAccountBalance();
+			record2.setCurrencyAmt(record.getCashSettleAmt());
+			record2.setMemCode(record.getDebitMemId());
+			record2.setHolderAccount(record.getDebitHolderAccount());
+			record2.setTitleCode(record.getCashDebitTitle());
+			record2.setCashAccount("0009");
+
+			kouKuan(cashSettleOrder.getSerialNum(), record2);
+
+		} catch (Exception e) {
 			e.printStackTrace();
-			logger.error("资金DVP结算请求指令异常:"+record);
+			logger.error("资金DVP结算请求指令异常:" + record);
 		}
-    }
-    
-    /* @Async("taskExecutor")*/
-    public void asynUpdateCashCount(CashDto record) {
-    	updateCashCount(record);
-    }
-     
-    /**
-     * 扣款
-     * @param serialNum
-     * @param record2
-     * @throws Exception
-     */
-    @Transactional
-    public void kouKuan(String serialNum,CashAccountBalance record2) throws Exception {
-    	boolean result = true;
-    	if(0 == cashAccountBalanceMapper.updateMoney(record2)){
-			logger.error("余额不足:"+record2);
+	}
+
+	/* @Async("taskExecutor") */
+	public void asynUpdateCashCount(CashDto record) {
+		updateCashCount(record);
+	}
+
+	/**
+	 * 扣款
+	 * 
+	 * @param serialNum
+	 * @param record2
+	 * @throws Exception
+	 */
+	@Transactional
+	public void kouKuan(String serialNum, CashAccountBalance record2) throws Exception {
+		boolean result = true;
+		if (0 == cashAccountBalanceMapper.updateMoney(record2)) {
+			logger.error("余额不足:" + record2);
 			CashSettleOrder record3 = new CashSettleOrder();
 			record3.setSerialNum(serialNum);
 			record3.setCashProcStatus("2");
 			cashSettleOrderMapper.updateByPrimaryKeySelective(record3);
 			result = false;
-		}else {
+		} else {
 			CashSettleOrder record3 = new CashSettleOrder();
 			record3.setSerialNum(serialNum);
 			record3.setCashProcStatus("1");
 			cashSettleOrderMapper.updateByPrimaryKeySelective(record3);
 			record2.setSerialNum(serialNum);
-			//cashSettleOrderMapper.updateStateSuccess(serialNum);
+			record2.setMsgType("HVPS.111.001.01");
+			// cashSettleOrderMapper.updateStateSuccess(serialNum);
+			logger.debug("发送Rabbit报文:" + objectMapper.writeValueAsString(record2));
 			amqpTemplate.convertAndSend("pay", objectMapper.writeValueAsString(record2));
-			logger.debug("交易成功:"+serialNum);
+			logger.debug("交易成功:" + serialNum);
 			result = true;
-		}	
-    	
-    	if(result){// 调用资金dep异步结算通知
-			
 		}
-    }
-    
-    public void pay(String serialNum) {
-    	CashSettleOrder record3 = new CashSettleOrder();
-		record3.setSerialNum(serialNum);
-		record3.setCashProcStatus("4");
-		cashSettleOrderMapper.updateByPrimaryKeySelective(record3);	
 		
+		if (result) {// 调用资金dep异步结算通知
+
+		}
+	}
+
+	public void pay(String serialNum) {
+		
+		CashSettleOrder cashSettleOrder = new CashSettleOrder();
+		
+		cashSettleOrder.setSerialNum(serialNum);
+		cashSettleOrder.setCashProcStatus("4");
+		
+		cashSettleOrderMapper.updateByPrimaryKeySelective(cashSettleOrder);
+
 		/**
 		 * 通知清算成功
 		 */
-       
-    }
+		cashSettleOrder = selectByPrimaryKey(cashSettleOrder.getSerialNum());
+		
+		CashSettleNotifyReq cashSettleNotifyReq = new CashSettleNotifyReq();
+		
+		cashSettleNotifyReq.setCashSettleId(cashSettleOrder.getCashSettleId());
+		cashSettleNotifyReq.setSettleOrderId(cashSettleOrder.getSettleOrderId());
+		cashSettleNotifyReq.setTradeId(cashSettleOrder.getTradeId());
+		cashSettleNotifyReq.setCashProcStatus("3");
+		cashSettleNotifyReq.setRetMsg("款项到账");
+		
+		settleNotifyClient.cashRsp(cashSettleNotifyReq);
+	}
 }
