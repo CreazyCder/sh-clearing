d9dc5dc8a72c47c8ec9422a3ae16ab88c83b3587
boip <boip@LAPTOP-JQ4B2MIH>
2019-11-22 17:11:24 +0800

    update gateway

--- renhang-gateway/src/main/java/cn/com/yusys/yusp/service/mq/PayMQListener.java
+++ renhang-gateway/src/main/java/cn/com/yusys/yusp/service/mq/PayMQListener.java
@@ -2,7 +2,9 @@ package cn.com.yusys.yusp.service.mq;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -21,49 +23,47 @@ import cn.com.yusys.yusp.service.MoneyDto;
 
 @Service
 public class PayMQListener {
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private Logger logger = LoggerFactory.getLogger(PayMQListener.class);
 
-	private ObjectMapper objectMapper = new ObjectMapper();
-	private Logger logger = LoggerFactory.getLogger(PayMQListener.class);
+    @Autowired
+    private AmqpTemplate amqpTemplate;
 
-	@Autowired
-	private AmqpTemplate amqpTemplate;
+    @Autowired
+    private CashClient cashClient;
 
-	@Autowired
-	private CashClient cashClient;
+    @RabbitListener(queues = {"to_shch"})
+    @RabbitHandler
+    public void receiveQueue(String message) {
+        System.out.println(message);
+        String msg = null;
+        try {
 
-	@RabbitListener(queues = { "payback" })
-	@RabbitHandler
-	public void receiveQueue(String message) {
-		System.out.println(message);
-		try {
+            String[] items = message.split(",");
+            byte[] bytes = new byte[items.length];
+            for (int i = 0; i < items.length; i++) {
+                bytes[i] = Byte.parseByte(items[i]);
+            }
+            msg = new String(bytes, "UTF-8");
+            Map data = objectMapper.readValue(msg, Map.class);
+            logger.info("支付处理接收报文格式化对象成功：" + msg);
 
-			/*
-			 * Map dd = new HashMap<String,String>(); dd.put("serialNum", "2222");
-			 * cashClient.pay(dd);
-			 */
+            if (data.get("msgType").equals("HVPS.111.001.01")) {
+                ResultDto<String> result = cashClient.pay(data);
+                logger.info("cash返回成功：" + result);
+            } else if (data.get("msgType").equals("HVPS.112.001.01")) {
+                MoneyDto money = new MoneyDto();
+                money.setCashAccount(data.get("cashAccount").toString());
+                money.setCurrencyAmt(new BigDecimal(data.get("currencyAmt").toString()));
+                money.setHolderAccount(data.get("holderAccount").toString());
+                money.setMemCode(data.get("memCode").toString());
+                money.setTitleCode(data.get("titleCode").toString());
+                cashClient.add(money);
+            }
 
-			Map data = objectMapper.readValue(message, Map.class);
-			logger.info("支付处理接收报文格式化对象成功：" + message);
-
-			if (data.get("msgType").equals("HVPS.111.001.01")) {
-				ResultDto<String> result = cashClient.pay(data);
-				logger.info("自动化测试平台(人行)返回成功：" + result);
-			} else if (data.get("msgType").equals("HVPS.112.001.01")) {//
-				// payback塞消息
-				//amqpTemplate.convertAndSend("payback", message);
-
-				MoneyDto money = new MoneyDto();
-				money.setCashAccount(data.get("cashAccount").toString());
-				money.setCurrencyAmt(new BigDecimal(data.get("currencyAmt").toString()));
-				money.setHolderAccount(data.get("holderAccount").toString());
-				money.setMemCode(data.get("memCode").toString());
-				money.setTitleCode(data.get("titleCode").toString());
-				cashClient.add(money);
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-			logger.info("支付处理接收报文格式化对象失败：" + message);
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.info("支付处理接收报文格式化对象失败：" + msg);
+        }
+    }
 }
--- renhang-gateway/src/main/java/cn/com/yusys/yusp/web/rest/CashResource.java
+++ renhang-gateway/src/main/java/cn/com/yusys/yusp/web/rest/CashResource.java
@@ -1,21 +1,43 @@
 package cn.com.yusys.yusp.web.rest;
 
+import cn.com.yusys.yusp.commons.exception.YuspException;
+import cn.com.yusys.yusp.commons.web.rest.dto.ResultDto;
+import cn.com.yusys.yusp.service.EnoughMoneyDto;
+import cn.com.yusys.yusp.service.MoneyDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import cn.com.yusys.yusp.commons.web.rest.dto.ResultDto;
+import javax.validation.Valid;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/cash")
+@RequestMapping("/api/gw")
 public class CashResource {
     private Logger logger = LoggerFactory.getLogger(CashResource.class);
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private AmqpTemplate amqpTemplate;
 
-    @GetMapping("/riqie")
-    protected ResultDto<String> create(String code) {
-    	logger.info("日切成功:"+code);
-        return new ResultDto<String>("0");
+    /**
+     * 通过Swagger发起人行来账报文到payback队列
+     * @param enoughMoneyDto
+     * @return
+     * @throws Exception
+     */
+    @PostMapping("/enough_money")
+    protected ResultDto<String> enoughMoney(@RequestBody EnoughMoneyDto enoughMoneyDto) throws Exception {
+        try {
+            String requestString = objectMapper.writeValueAsString(enoughMoneyDto);
+            logger.info("发送到款数据到Rabbit:" + requestString);
+            amqpTemplate.convertAndSend("to_hvps", requestString);
+        } catch (Exception e) {
+            throw new YuspException("1", "发送rabbitmq失败" + e.getMessage());
+        }
+        return new ResultDto<String>(0, "到款处理成功", "ok");
     }
 }
